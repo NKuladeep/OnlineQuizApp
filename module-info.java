751a9665067fd5f77f5c30c5e9928d725277c2e2
Nageti. Kuladeep <139630947+NKuladeep@users.noreply.github.com>
2025-09-12 21:00:09 +0530

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,9 @@
+// module-info.java
+module quizapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
+    
+    exports com.quizapp;
+}
